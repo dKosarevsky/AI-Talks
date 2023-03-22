a9faf3e8a8a34cd5f70247ab421dd24fe0deb150
dKosarevsky <36589322+dKosarevsky@users.noreply.github.com>
2023-03-23 00:39:30 +0300

    Update chat.py
    w in px

--- chat.py
+++ chat.py
@@ -49,7 +49,7 @@ selected_lang = option_menu(
             "margin": "auto",
             "background-color": "#2C3333",
             "height": "30px",
-            "width": "6rem",
+            "width": "50px",
             "color": "#CBE4DE",
             "border-radius": "5px"
         },
