78faf368b1b9f7d27847c4eb23d08d6cccd9255a
if <if.kosarevsky@gmail.com>
2023-04-28 23:34:27 +0300

    upd footer

--- ai_talks/src/utils/footer.py
+++ ai_talks/src/utils/footer.py
@@ -16,7 +16,7 @@ def show_info(icon: Path) -> None:
         - {render_svg(icon)} [{st.session_state.locale.footer_channel}](https://t.me/talks_aii)
     """, unsafe_allow_html=True)
     st.divider()
-    st.markdown("[project repo](https://github.com/dKosarevsky/AI-Talks)")
+    st.markdown("project [repo on github](https://github.com/dKosarevsky/AI-Talks) waiting for your :star:")
 
 
 def show_donates() -> None:
