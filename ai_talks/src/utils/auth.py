e49f8b0daa50015cd106f67ea7dc12082a16846c
if <if.kosarevsky@gmail.com>
2023-05-08 00:31:00 +0300

    add tg & fix err handler

--- ai_talks/src/utils/auth.py
+++ ai_talks/src/utils/auth.py
@@ -1,7 +1,7 @@
 import time
 
-import requests
 import streamlit as st
+from requests import exceptions, post
 from streamlit_option_menu import option_menu
 
 from ..styles.menu_styles import HEADER_STYLES
@@ -22,25 +22,30 @@ def login(applicant_token: str) -> None:
         st.session_state.authentication_status = True
     else:
         with st.form("my_form"):
-            email = st.text_input(label="email")
+            username = st.text_input(label="username")
             password = st.text_input(label="password", type="password")
+            data = {"username": username, "password": password}
             if st.form_submit_button(label="Login"):
                 with st.spinner("login..."):
-                    response = requests.post(url=st.secrets.back.base_url + "auth/", headers=HEADERS,  # noqa: S113
-                                             json={"email": email, "password": password},
-                                             auth=get_back_auth())
-                    if response.status_code == 200:
-                        response_json = response.json()
-                        applicant_token = response_json["token"]
-                        st.session_state.username = response_json["username"]
-                        st.session_state.user_tokens = response_json["ai_tokens"]
-                        if applicant_token:
-                            st.session_state.key = "applicant-token"
-                            st.session_state["applicant-token"] = applicant_token
-                            st.session_state.authentication_status = True
-                            st.experimental_rerun()
-                    else:
-                        st.error(f"Error: {response.text}. Status code: {response.status_code}")
+                    try:
+                        response = post(url=st.secrets.back.base_url + "auth/", headers=HEADERS,  # noqa: S113
+                                        json=data, auth=get_back_auth())
+                        if response.status_code == 200:
+                            response_json = response.json()
+                            applicant_token = response_json["token"]
+                            st.session_state.username = response_json["username"]
+                            st.session_state.user_tokens = response_json["ai_tokens"]
+                            if applicant_token:
+                                st.session_state.key = "applicant-token"
+                                st.session_state["applicant-token"] = applicant_token
+                                st.session_state.authentication_status = True
+                                st.experimental_rerun()
+                        elif response.status_code in range(500, 512):
+                            st.error(f"Server Error. Status code: {response.status_code}")
+                        else:
+                            st.error(f"Error: {response.text}. Status code: {response.status_code}")
+                    except exceptions.RequestException as err:
+                        st.error(f"Request Error: {err}")
 
 
 def register(applicant_token: str) -> None:
@@ -54,18 +59,24 @@ def register(applicant_token: str) -> None:
                 st.experimental_rerun()
     else:
         with st.form("my_form"):
+            telegram = st.text_input(label="telegram")
             email = st.text_input(label="email")
             username = st.text_input(label="username")
             password = st.text_input(label="password", type="password")
+            data = {"telegram": telegram, "email": email, "username": username, "password": password}
             if st.form_submit_button(label="Register"):
                 with st.spinner("registration..."):
-                    response = requests.post(url=st.secrets.back.base_url + "register/", headers=HEADERS,  # noqa: S113
-                                             json={"email": email, "username": username, "password": password},
-                                             auth=get_back_auth())
-                    if response.status_code == 200:
-                        st.experimental_rerun()
-                    else:
-                        st.error(f"Error: {response.text}. Status code: {response.status_code}")
+                    try:
+                        response = post(url=st.secrets.back.base_url + "register/", headers=HEADERS,  # noqa: S113
+                                        json=data, auth=get_back_auth())
+                        if response.status_code == 200:
+                            st.experimental_rerun()
+                        elif response.status_code in range(500, 512):
+                            st.error(f"Server Error. Status code: {response.status_code}")
+                        else:
+                            st.error(f"Error: {response.text}. Status code: {response.status_code}")
+                    except exceptions.RequestException as err:
+                        st.error(f"Request Error: {err}")
 
 
 def show_auth_menu() -> None:
