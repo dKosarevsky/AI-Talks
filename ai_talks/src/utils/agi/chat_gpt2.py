a7ed9436d21c379b066393e35d3f8fa527bf9028
if <if@>
2024-01-08 22:10:00 +0300

    clear create_gpt_completion

--- ai_talks/src/utils/agi/chat_gpt2.py
+++ ai_talks/src/utils/agi/chat_gpt2.py
@@ -2,6 +2,8 @@ import logging
 import streamlit as st
 
 from typing import List  # NOQA: UP035
+
+from openai.types.chat import ChatCompletion
 from tenacity import retry, stop_after_attempt
 
 from .api_utils import get_api_client
@@ -9,7 +11,7 @@ from .api_utils import get_api_client
 
 @st.cache_data()
 @retry(stop=stop_after_attempt(3))
-def create_gpt_completion(ai_model: str, messages: List[dict]) -> dict:
+def create_gpt_completion(ai_model: str, messages: List[dict]) -> ChatCompletion:
     client = get_api_client()
     logging.info(f"{messages=}")
     # https://platform.openai.com/docs/api-reference/chat/create
@@ -17,12 +19,7 @@ def create_gpt_completion(ai_model: str, messages: List[dict]) -> dict:
         model=ai_model,
         messages=messages,
         temperature=st.session_state.temperature,
-        # top_p=st.session_state.top_p,
-        # max_tokens=st.session_state.max_tokens,
         n=1,
-        # stop="",
-        # presence_penalty=st.session_state.presence_penalty,
-        # frequency_penalty=st.session_state.frequency_penalty,
         user=st.session_state.username,
         # stream=True,
     )
