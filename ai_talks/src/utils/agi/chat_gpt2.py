2a88232f0e3809b4857dfa1782e049668f4a7fe3
if <if@>
2025-01-25 00:47:54 +0300

    handle errors

--- ai_talks/src/utils/agi/chat_gpt2.py
+++ ai_talks/src/utils/agi/chat_gpt2.py
@@ -4,6 +4,7 @@ import streamlit as st
 from typing import List  # NOQA: UP035
 
 from openai.types.chat import ChatCompletion
+from openai import OpenAIError
 from tenacity import retry, stop_after_attempt
 
 from .api_utils import get_api_client
@@ -15,13 +16,17 @@ def create_gpt_completion(ai_model: str, messages: List[dict]) -> ChatCompletion
     client = get_api_client()
     logging.info(f"{messages=}")
     # https://platform.openai.com/docs/api-reference/chat/create
-    completion = client.chat.completions.create(
-        model=ai_model,
-        messages=messages,
-        temperature=st.session_state.temperature,
-        n=1,
-        user=st.session_state.username,
-        # stream=True,
-    )
+    try:
+        completion = client.chat.completions.create(
+            model=ai_model,
+            messages=messages,
+            temperature=st.session_state.temperature,
+            n=1,
+            user=st.session_state.username,
+            # stream=True,
+        )
+    except OpenAIError as err:
+        st.error(err)
+        st.stop()
     logging.info(f"{completion=}")
     return completion
